72b01a9cbe2204519ab50ef51ca865900ebe969d
Jack Machesky <machesk7@msu.edu>
2023-05-01 00:43:21 -0400

    updated

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = 'https://zygnciikdyabufikydye.supabase.co';
+const supabaseUrl = 'https://gfraeduaqdnaqtjvjrsz.supabase.co';
 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5Z25jaWlrZHlhYnVmaWt5ZHllIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNDEzMzIsImV4cCI6MTk5MzcxNzMzMn0.8SkCh6dCUtrC2KGOJxLRcgGsB8fODh1ZkbFYlvBhphA';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmcmFlZHVhcWRuYXF0anZqcnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5MDczMzEsImV4cCI6MTk5ODQ4MzMzMX0.juk6SesWBQ_ie_CaUeg0bZ2lVuN8AMAOReQ3BF3qa34';
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
